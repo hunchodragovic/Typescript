1e5f4977adb8fb67f472741d39b89de6107aa50e
Ilyes <mingo.huncho@gmail.com>
2024-11-27 23:10:45 +0100

    fetch api

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+const myPromise: Promise<string> = new Promise((resolve, reject) => {
+    // Simulate an asynchronous operation
+    setTimeout(() => {
+      const success = true; // Change this to false to test rejection
+      if (success) {
+        resolve("The task succeeded!");
+      } else {
+        reject("The task failed.");
+      }
+    }, 2000);
+  }).then((result) => {
+    console.log(result); // Output: "The task succeeded!" (after 2 seconds)
+  }) .catch((error) => {
+    console.error(error); // Output: "The task failed." (if it fails)
+  });;
+  
